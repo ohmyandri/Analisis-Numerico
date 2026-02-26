0eeb857f4015310d9907809132fa3ca93a9dbcda
Andrick <avilaandrick8@gmail.com>
2026-02-26 15:37:41 -0600

    Newton Raphson

--- Metodos Numericos Cerrados/Biseccion/main.py
+++ Metodos Numericos Cerrados/Biseccion/main.py
@@ -14,10 +14,10 @@ def funcionDeX(x):
 def funcionXm(a, b):
     return (a+b)/2
 
-#Aplicacion del metodo Numerico
+#Aplicacion del metodo Numerico con Ciclo for
 def metodoNumerico(a, b): #pasamos valores iniciales
     xa = a
-    for i in range(1, 100+1):
+    for i in range(1, maxIter):
         #calculo de xm
         Xm = funcionXm(a, b)
         error = abs(xa - Xm)
@@ -42,5 +42,39 @@ def metodoNumerico(a, b): #pasamos valores iniciales
     print(f"Error: {error}")
     print(f"La raiz aproximada en la iteracion es Xm con valor de: {round(Xm, cifrasDecimales)}")
 
-metodoNumerico(0, 2)
+#Aplicacion del metodo Numerico con ciclo While, permitiendo mantener la condicion de parada como el criterio para frenar independientemente del numero de iteracion como en el for
+def metodoNumericoWhile(a, b): #pasamos valores iniciales
+    #Bandera que indicara la iteracion
+    i = 0
+    xa = a
+    error = 1e10
+    while(error > epsilon):
+        #Aumento en la bandera
+        i += 1
+
+        #calculo de xm
+        Xm = funcionXm(a, b)
+        error = abs(xa - Xm)
+        xa = Xm
+
+        if(funcionDeX(a) * funcionDeX(Xm) < 0): #hay cambio de signo, por tanto, la raiz esta entre esos
+            # print("El cambio de signo esta entre f(a) y f(Xm)")
+            b = Xm
+
+        elif(funcionDeX(b) * funcionDeX(Xm) < 0): #hay cambio de signo, por tanto, la raiz esta entre esos
+            a = Xm
+            # print("El cambio de signo esta entre f(b) y f(Xm)")
+        
+        # else:
+        #     print(f"La raiz es Xm con valor de: {Xm}")
+        #     break
+
+        if(error < epsilon):
+            print(f"Iteracion numero {i}")
+            break
+
+    print(f"Error: {error}")
+    print(f"La raiz aproximada en la {i} iteracion es Xm con valor de: {round(Xm, cifrasDecimales)}")
+
+# metodoNumericoWhile(0, 2)
     
--- Metodos Numericos Cerrados/Newton-Raphson/main.py
+++ Metodos Numericos Cerrados/Newton-Raphson/main.py
@@ -1,5 +1,37 @@
-import numpy as np
-xo = 1
+import numpy as np # type: ignore
 
-def funcionDeX(x):
-    return ((np.exp(x)) + x - 2)
+#Condiciones de parada
+epsilon = 1e-3
+
+#Definimos la funcion para valuar en la funcion
+def f(x):
+    return ((np.exp(-x)) - x + 2)
+
+#Definimos la funcion para valuar en la f prima
+def fp(x):
+    return ( -(np.exp(-x)) - 1 )
+
+def metodoNumerico(a): #a es el valor inicial
+    #Condiciones Iniciales y valor a buscar
+    x0 = 0 #En esta variable guardaremos el valor anterior digamos es la x (i - 1)
+    xi = a
+
+    #Error actual, iniciado en 1e10 para poder iniciar el ciclo
+    error = 1e10
+
+    #Mientras nuestro error, sea mayor o igual a la tolerancia que pusimos, es decir continuara sucediendo asta que el error sea menor a la condicion de parada
+    while(error >= epsilon):
+        # xi = x0 #Marcamos a xi como el punto en que estamos
+        xi = xi - (f(xi))/(fp(xi))
+
+        #Calculamos el error
+        error = abs(xi - x0)
+
+        #Guardamos el xi que teniamos
+        x0 = xi
+
+        #Calculamos el nuevo xi
+
+        print(xi)
+
+metodoNumerico(1)
